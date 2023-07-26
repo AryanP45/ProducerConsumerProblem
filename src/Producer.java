c184264249aad0387c7e7963a15797dbe600b63a
Aryan <aryanitinpatil@gmail.com>
2023-07-26 10:04:33 +0530

    Producer Module

---
+++ src/Producer.java
@@ -0,0 +1,16 @@
+public class Producer extends Thread {
+    Factory f;
+    Producer(Factory f){
+        this.f=f;
+    }
+    public void run(){
+        int i=0;
+            while(i<20){
+                f.produce(i);
+                try{
+                    Thread.sleep(1000);
+                }catch(Exception e){}
+                i++;
+            }
+    }
+}
