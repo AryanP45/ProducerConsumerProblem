b2031dc2f747c83e319a4201f02a979c32cf4d49
Aryan <aryanitinpatil@gmail.com>
2023-07-26 10:04:09 +0530

    Consumer Module

---
+++ src/Consumer.java
@@ -0,0 +1,18 @@
+public class Consumer extends Thread{
+    Factory f;
+    Consumer(Factory f){
+        this.f = f;
+    }
+
+    public void run(){
+        int i=0;
+        while(i<20){
+            f.consume();
+            try{
+                Thread.sleep(2000);
+            }catch(Exception e){}
+            i++;
+        }
+    }
+
+}
